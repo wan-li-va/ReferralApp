14eb2efc1df89ba3b0175726e1e79eb01f587be3
Wan Li <wl9wgc@virginia.edu>
2020-06-02 16:07:17 -0400

    add link to dashboard

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,7 @@ const Header = () => {
             <h4 className="Subtitle">Responsibly Show Hoo Owns the Environment</h4>
             <Link className="HeaderLink" to="/ReferralApp/about">About Us</Link>
             <Link className="HeaderLink" to="/ReferralApp/faq">FAQ</Link>
+            <Link className="HeaderLink" to="/ReferralApp/dashboard">Dashboard</Link>
         </div>
     )
 }
