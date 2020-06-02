1a4ad2528f22317c6d8ea2aa9897bbfc1e3f34ae
Coleman Jenkins <ctj9qgq@virginia.edu>
2020-06-02 17:28:37 -0400

    Create basic FAQ page

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 const faq = () => {
   return (
-    <div>FAQ</div>
+    <div>
+      <h1>FAQ</h1>
+      <h4>Q: How do I get points?</h4>
+      <p>
+        A: One referral gets you one point,
+        and sharing on socoal media also gets
+        you one point.
+        </p>
+      <h4>Q: What counts as a referral?</h4>
+      <p>
+        A: When someone makes an account using your
+        referral code.
+      </p>
+      <h4>Q: I shared on social media, why didn't I get a point?</h4>
+      <p>
+        A: You can only get a point from sharing from social
+        media once, so once you've already shared you can't get
+        anymore points.
+      </p>
+    </div>
   );
 }
 export default faq;
