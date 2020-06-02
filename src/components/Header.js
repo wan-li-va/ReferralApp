cdb2f4223d5d8dbd6e4a712405e7d19ceff06cbb
Wan Li <wl9wgc@virginia.edu>
2020-06-02 15:12:38 -0400

    set up react router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import '../styling/Header.css'
 
 const Header = () => {
     return (
         <div>
-            Header
+            <h1>ReSHOE</h1> 
+            <h3>Responsibly Show Hoo Owns the Environment</h3>
+            <Link to="/ReSHOE/about">About Us</Link>
+            <Link to="/ReSHOE/faq">FAQ</Link>
         </div>
     )
 }
