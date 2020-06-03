e64a701b1e8880f886b1880067940f2f7531a4b5
Dakota Bourne <44448765+dakotabourne373@users.noreply.github.com>
2020-06-03 12:32:01 -0400

    added referral code functionality

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,66 +1,125 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Button from 'react-bootstrap/Button';
 import { withFirebase } from './Firebase';
 import Form from 'react-bootstrap/Form';
 import { Redirect } from 'react-router-dom';
+import Overlay from 'react-bootstrap/Overlay';
+import Tooltip from 'react-bootstrap/ToolTip';
 
 const Auth = props => {
     const [email, setEmail] = useState("");
+    const [isNewUser, setIsNewUser] = useState(false);
+    const [show, setShow] = useState(false);
+    const [signedIn, setSignedIn] = useState(false);
+    const target = useRef(null);
 
     const handleSubmit = (e) => {
         const { firebase } = props;
         e.preventDefault();
         let email = e.target.elements.formEmail.value;
         setEmail(email);
-        firebase.user(1).once('value')
-            .then(snapshot => {
-                let userArr = snapshot.val();
-                let user = userArr.filter(userObj => userObj.email === email)
-                if (user[0]) {
-                    handleOldUser(user[0].id);
-                } else {
-                    handleNewUser(email);
-                }
-            })
+        if (!isNewUser)
+            firebase.user(1).once('value')
+                .then(snapshot => {
+                    let userArr = snapshot.val();
+                    let user = userArr.filter(userObj => userObj.email === email)
+                    if (user[0]) {
+                        handleOldUser(user[0].id);
+                    } else {
+                        setIsNewUser(true);
+                    }
+                })
+        else {
+            let refCode = e.target.elements.formReferral.value;
+            checkRefCode(email, refCode);
+        }
 
     }
 
     const handleOldUser = (uid) => {
         props.setUser(uid);
-        return <Redirect to='/Dashboard' />
+        setSignedIn(true);
     }
 
-    const handleNewUser = (email) => {
+    const checkRefCode = (email, refCode) => {
         const { firebase } = props;
-        let postRef = firebase.users().push({
-            email: email
-        })
-        let userObj = { email: email, id: postRef.key, referralCode: postRef.key.substring(3), numReferrals: 0, hasShared: false, rewards: { 0: 'dummy' } };
-        firebase.user(postRef.key)
-            .set(userObj)
         firebase.user(1).once('value')
             .then(snapshot => {
-                let users = snapshot.val();
-                let userArr = [];
-                for (let key in users) {
-                    userArr.push(users[key])
+                let userArr = snapshot.val();
+                let user = userArr.filter(userObj => userObj.id.substring(3) === refCode);
+                if (user[0]) {
+                    let id = user[0].id
+                    firebase.user(id).once('value')
+                        .then(snapshot => {
+                            let userObj = snapshot.val();
+                            firebase.user(id).update({
+                                numReferrals: userObj.numReferrals + 1,
+                                hasShared: true
+                            })
+                        })
+                    setShow(false);
+                    handleNewUser(email, false);
+                } else {
+                    setShow(true);
                 }
-                userArr = [...userArr, { email: email, id: postRef.key }];
-                firebase.user(1).update(userArr);
             })
-        props.setUser(postRef.key);
-        return <Redirect to='/Dashboard' />
     }
+
+    const handleNewUser = (email, error) => {
+        const { firebase } = props;
+        if (!error) {
+            let postRef = firebase.users().push({
+                email: email
+            })
+            let id = postRef.key
+            let userObj = { email: email, id: id, referralCode: id.substring(3), numReferrals: 0, hasShared: false, rewards: { 0: 'dummy' } };
+            firebase.user(id)
+                .set(userObj)
+            firebase.user(1).once('value')
+                .then(snapshot => {
+                    let users = snapshot.val();
+                    let userArr = [];
+                    for (let key in users) {
+                        userArr.push(users[key])
+                    }
+                    userArr = [...userArr, { email: email, id: id }];
+                    firebase.user(1).update(userArr);
+                })
+            props.setUser(id);
+            setSignedIn(true);
+        }
+    }
+
+    const renderNewUserForm = () => (
+        <Form onSubmit={event => (handleSubmit(event))}>
+            <Form.Group controlId='formEmail' >
+                <Form.Label>Enter your email address!</Form.Label>
+                <Form.Control type='email' placeholder='Enter email' defaultValue={email} />
+            </Form.Group>
+            <Form.Group controlId='formReferral'>
+                <Form.Label>Enter a referral code if applicable</Form.Label>
+                <Form.Control type='text' placeholder='Enter Referral Code' ref={target} />
+                <Overlay target={target.current} show={show} placement='bottom'>
+                    {props => (<Tooltip id='overlay-error' {...props}>
+                        Please put in a correct referral code
+                    </Tooltip>)}
+                </Overlay>
+            </Form.Group>
+            <Button variant='primary' type='submit'>Submit!</Button>
+        </Form>
+    );
 
     return (
         <div>
-            <Form onSubmit={event => (handleSubmit(event))}>
-                <Form.Group controlId='formEmail' >
-                    <Form.Label>Enter your email address!</Form.Label>
-                    <Form.Control type='email' placeholder='Enter email' />
-                </Form.Group>
-                <Button variant='primary' type='submit'>Submit!</Button>
-            </Form>
+            {signedIn ? <Redirect to='/dashboard' />
+                : isNewUser ? renderNewUserForm()
+                    : <Form onSubmit={event => (handleSubmit(event))}>
+                        <Form.Group controlId='formEmail' >
+                            <Form.Label>Enter your email address!</Form.Label>
+                            <Form.Control type='email' placeholder='Enter email' />
+                        </Form.Group>
+                        <Button variant='primary' type='submit'>Submit!</Button>
+                    </Form>}
         </div>
     );
 }
