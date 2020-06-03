c302d7f0743ecec34163b14da1a3206f2c5a9db8
Coleman Jenkins <ctj9qgq@virginia.edu>
2020-06-03 11:01:53 -0400

    Hooks refactoring and styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import ProgressBar from './Dashboard/ProgressBar.js';
 import ReferralDisplay from './Dashboard/ReferralDisplay.js';
 import Rewards from './Dashboard/Rewards.js';
@@ -6,72 +6,43 @@ import Social from './Dashboard/Social.js';
 import NextReward from './Dashboard/NextReward.js';
 import '../styling/Dashboard.css';
 
-export default class Dashboard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            numReferrals: 8,
-            nextAchievement: 10,
-            referralCode: "",
-            rewards: {},
-            hasShared: false,
+const Dashboard = ({ userID, firebase }) => {
+    const [numReferrals, setNumReferrals] = useState(8);
+    const [nextAchievement, setNextAchievement] = useState(10);
+    const [referralCode, setReferralCode] = useState("");
+    const [rewards, setRewards] = useState({});
+    const [hasShared, setHasShared] = useState(false);
+
+    // useEffect(
+    //    firebase.user(userID) 
+    // , [])
+
+    const handleSocialShare = () => {
+        if (!hasShared) {
+            setNumReferrals(numReferrals + 1);
+            setHasShared(true);
         }
     }
 
-    /*  Person info
-        "id": {
-            referralCode,
-            numReferrals,
-            hasShared,
-            email,
-            name
-            //stretch: address, referrals
-        }
-    */
-
-    componentDidMount = () => {
-        /*
-        from firebase:
-        get current user
-            id
-        get data we need
-        */
-    }
-
-    handleSocialShare = () => {
-        this.setState(prevState => {
-            if (!prevState.hasShared)
-                return {
-                    numReferrals: prevState.numReferrals + 1,
-                    hasShared: true
-                }
-        })
-    }
-
-    render() {
-        const {
-            userID
-        } = this.props;
-
-        return (
-            <div className="Dashboard">
-                <Rewards classname="Rewards" rewards={this.state.rewards} />
-                <div className="displayMainPanel">
-                    <ReferralDisplay className="ReferralDisplay" code={this.state.referralCode} />
-                    <Social
-                        handleSocialShare={this.handleSocialShare}
-                        referralCode={this.state.referralCode}
-                        hasShared={this.state.hasShared} />
-                    <div className="displayProgress">
-                        <ProgressBar
-                            numReferrals={this.state.numReferrals}
-                            nextAchievement={this.state.nextAchievement} />
-                        <NextReward
-                            nextAchievement={this.state.nextAchievement}
-                            rewards={this.state.rewards} />
-                    </div>
+    return (
+        <div className="Dashboard">
+            <Rewards classname="Rewards" rewards={rewards} />
+            <div className="displayMainPanel">
+                <ReferralDisplay className="ReferralDisplay" code={referralCode} />
+                <Social
+                    handleSocialShare={handleSocialShare()}
+                    referralCode={referralCode}
+                    hasShared={hasShared} />
+                <div className="displayProgress">
+                    <ProgressBar
+                        numReferrals={numReferrals}
+                        nextAchievement={nextAchievement} />
+                    <NextReward
+                        nextAchievement={nextAchievement}
+                        rewards={rewards} />
                 </div>
             </div>
-        )
-    }
+        </div>
+    );
 }
+export default Dashboard;
