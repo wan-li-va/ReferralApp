5962b11faefcc67d346365b2ecef62f18d96f13b
Coleman Jenkins <ctj9qgq@virginia.edu>
2020-06-05 10:34:08 -0400

    Redesign admin page

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { withFirebase } from './Firebase';
-import Form from 'react-bootstrap/Form'
+import Card from 'react-bootstrap/Card';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styling/AdminPage.css'
 
 const AdminPage = (props) => {
     const [isGlobalAdmin, setGlobalAdmin] = useState(false);
@@ -26,8 +33,8 @@ const AdminPage = (props) => {
         })
     }, [])
 
-    const handleUserChange = (user) => {
-        const person = user.target.value;
+    const handleUserChange = (id) => {
+        const person = id;
         setSelectedUser(person);
         setEditedObject(users[person]);
     }
@@ -50,37 +57,72 @@ const AdminPage = (props) => {
     }
 
     return (
-        <div className="page">
-            Global Admin: {isGlobalAdmin ? "true" : "false"}
-            <Form.Control as="select" multiple onChange={handleUserChange}>
-                {users &&
-                    Object.values(users).map(obj => {
-                        return (
-                            <option style={{ height: "3vh" }} value={obj.id}
-                            >
-                                {obj.email}
-                            </option>
-                        );
-                    })}
-            </Form.Control>
-            <div>
-                {selectedUser && users && Object.keys(editedObject).map(key => {
-                    console.log(editedObject);
-                    console.log(editedObject[key]);
-                    if (key !== 'id')
-                        return (
-                            <div>
-                                <label>{key}</label>
-                                <input type="text" value={editedObject[key]}
-                                    onChange={(e) => changeInputHandler(e, key)} />
-                            </div>
-                        )
-                })}
+        <div className="page" >
+            <h2 style={{ textAlign: "center" }}>Admin Page</h2>
+            Permissions: {isGlobalAdmin ? "Global Admin" : "General Admin"}
+            {/* <br />Click on user to see information
+            <br />Click 'Edit User' to edit the user */}
+            <div style={{ display: "flex", alignContent: "center", justifyContent: "center" }} >
+                <div style={{ flex: 1, overflow: "scroll", maxHeight: "70vh" }}>
+                    {/* <Form.Control as="select" htmlSize={10} onChange={handleUserChange}> */}
+                    {users &&
+                        Object.values(users).map(obj => {
+                            return (
+                                <ExpansionPanel  >
+                                    <ExpansionPanelSummary
+                                        expandIcon={<ExpandMoreIcon />}
+                                        aria-controls="panel1a-content"
+                                        id="panel1a-header">
+                                        <Typography className="heading">
+                                            {obj.email}
+                                        </Typography>
+                                    </ExpansionPanelSummary>
+                                    <ExpansionPanelDetails className="panel">
+                                        <Typography>
+                                            {Object.keys(obj).map(key => {
+                                                console.log(editedObject);
+                                                console.log(editedObject[key]);
+                                                if (key !== 'id')
+                                                    return (
+                                                        <div>
+                                                            <b>{key}:</b> {obj[key]}
+                                                            {/* <label>{key}</label>
+                                                    <li type="text" value={obj[key]}
+                                                        onChange={(e) => changeInputHandler(e, key)} >
+                                                        {obj[key]}
+                                                    </li> */}
+                                                        </div>
+                                                    )
+                                            })}
+                                            <button onClick={() => handleUserChange(obj.id)}>Edit User</button>
+                                        </Typography>
+                                    </ExpansionPanelDetails>
+                                </ExpansionPanel>
+                            );
+                        })}
+                </div>
+                {/* </Form.Control> */}
+                {selectedUser &&
+                    <div style={{ flex: 1, display: "flex", justifyContent: "center" }}>
+                        <div>
+                            {selectedUser && users && Object.keys(editedObject).map(key => {
+                                console.log(editedObject);
+                                console.log(editedObject[key]);
+                                if (key !== 'id')
+                                    return (
+                                        <div>
+                                            <label>{key}</label>
+                                            <input type="text" value={editedObject[key]}
+                                                onChange={(e) => changeInputHandler(e, key)} />
+                                        </div>
+                                    )
+                            })}
+                            {selectedUser && <button onClick={handleEditUser}>Save Edit</button>}
+                            <button style={{ backgroundColor: "red" }} onClick={handleDeleteUser}>Delete User</button>
+                            <button onClick={handleClearSelectedUser}>Clear Selected User</button>
+                        </div>
+                    </div>}
             </div>
-            {/* {!selectedUser && <button onClick={handleCreateUser}>Create New User</button>} */}
-            {selectedUser && <button onClick={handleEditUser}>Save Edit</button>}
-            <button onClick={handleDeleteUser}>Delete User</button>
-            <button onClick={handleClearSelectedUser}>Clear Selected User</button>
         </div>
     )
 }
